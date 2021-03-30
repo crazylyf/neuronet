5089d7b2a0de4a2ba8499064cc9179547cee6d53
crazylyf <crazylyf@163.com>
2021-03-30 06:05:02 -0400

    initial commit, network part finished

---
+++ train.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+#================================================================
+#   Copyright (C) 2021 Yufeng Liu (Braintell, Southeast University). All rights reserved.
+#   
+#   Filename     : train.py
+#   Author       : Yufeng Liu
+#   Date         : 2021-03-19
+#   Description  : 
+#
+#================================================================
+
+import os
+import sys
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from neuronet.models import unet
+from neuronet.utils import util
+
+
+parser = argparse.ArgumentParser(
+    description='Segmentator for Neuronal Image With Pytorch')
+parser.add_argument('--train_set', 
+                    type=str, help='training set path')
+parser.add_argument('--val_set', 
+                    type=str, help='val set path')
+parser.add_argument('--batch_size', default=2, type=int,
+                    help='Batch size for training')
+parser.add_argument('--num_workers', default=4, type=int,
+                    help='Number of workers used in dataloading')
+parser.add_argument('--cuda', action="store_true", 
+                    help='Whether use gpu to train model, default True')
+parser.add_argument('--amp', action="store_true", 
+                    help='Whether to use AMP training, default True')
+parser.add_argument('--lr', '--learning-rate', default=1e-2, type=float,
+                    help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float,
+                    help='Momentum value for optim')
+parser.add_argument('--weight_decay', default=5e-4, type=float,
+                    help='Weight decay for SGD')
+parser.add_argument('--save_folder', default='weights/',
+                    help='Directory for saving checkpoint models')
+args = parser.parse_args()
+
+
+def train():
+    # initialize device
+    if args.cuda:
+        device = util.init_device('cuda')
+    else:
+        device = util.init_device('cpu')
+
+    # for output folder
+    if not os.path.exists(args.save_folder):
+        os.mkdir(args.save_folder)
+
+    # dataset preparing
+    train_set = ''
+    val_set = ''
+    train_loader = ''
+    val_loader = ''
+
+    # network initialization
+    net = ''    # explicit initialize
+
+    net = net.to(device)
+
+    # optimizer & loss
+    optimizer = optim
+    criterion = ''
+    criterion = criterion.to(device)
+
+    # training process
+    net.train()
+    
+    for 
+
+
+if __name__ == '__main__':
+    train()
+
+
+
+
+
+
